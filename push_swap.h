ac1ac7b2df12fd8c1f534e0c55726976f4e6e524
Aloik1 <ilykond@gmail.com>
2024-10-15 19:46:18 +0200

    Add files via upload

---
+++ push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikondrat <ikondrat@student.42madrid.com>   #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-10-15 15:43:28 by ikondrat          #+#    #+#             */
+/*   Updated: 2024-10-15 15:43:28 by ikondrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/libft.h"
+
+int	create_lists(char **argv, int i);
+
+#endif
